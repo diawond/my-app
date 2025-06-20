66d85110f2c4e3c974b6f9bae6ccaefbf8cc1dd0
diamond <pongsatron.pd@gmail.com>
2025-06-21 01:21:51 +0700

    v1

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>hello/index.tsx to edit this screen.</Text>
     </View>
   );
 }
